4ccf99134eaf6f4a7a82531d6bbda749cdb16500
Chenning Li <92351916+LiCning@users.noreply.github.com>
2022-03-24 23:43:36 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import app
+import os
+import pymongo
+
+#os.system(r'cd C:\Users\lenovo\Desktop\毕业设计\code\app\crawler && scrapy crawl toplist')
+
+# compare normal
+# db['compare_normal']   db['compare_n']
+
+app.run(debug = True)
